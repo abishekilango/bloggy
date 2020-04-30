3c96c0556e256cccf7445d5c71e9dcf56e2b0485
abishek <abishekmagudapathy@gmail.com>
2020-04-30 23:42:40 +0530

    Added bcrypt

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 exports.test = (req, res) => {
 	res.send('Greetings frm test controller');
@@ -8,9 +9,10 @@ exports.user_create = async (req, res) => {
 	try {
 		let user = await User.findOne({ username: req.query.username });
 		if (!user) {
+			const hashedPassword = await bcrypt.hash(req.query.password, 10);
 			user = new User({
 				username: req.query.username,
-				password: req.query.password,
+				password: hashedPassword,
 			});
 			await user.save();
 			res.status(201).send('User created');
@@ -36,10 +38,10 @@ exports.user_checkValidity = async (req, res) => {
 		const user = await User.findOne({ username: req.query.username });
 		if (!user) {
 			res.send('User data not found');
-		} else if (user.password !== req.query.password) {
-			res.send('Invalid credentials');
-		} else {
+		} else if (await bcrypt.compare(req.query.password, user.password)) {
 			res.status(200).send('User data valid');
+		} else {
+			res.send('Invalid credentials');
 		}
 	} catch (err) {
 		console.log(err);
